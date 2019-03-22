27173ab443e5a1c21d23a726c1f0aebc835407d9
duruliu72 <duruliu72@gmail.com>
2019-03-22 14:14:33 +0600

    Change Server folder

--- gulpfile.js
+++ gulpfile.js
@@ -8,31 +8,31 @@ mixins=require('postcss-mixins'),
 browserSync=require('browser-sync').create();
 
 function styles(){
-  return gulp.src('./app/assets/styles/styles.css')
+  return gulp.src('./docs/assets/styles/styles.css')
     .pipe(postcss([cssImport,mixins,cssvars,nested,autoprefixer]))
     .on('error',function(errorInfo){
       console.log(errorInfo.toString());
       this.emit('end');
     })
-    .pipe(gulp.dest('./app/temp/styles'));
+    .pipe(gulp.dest('./docs/temp/styles'));
 }
 
 function cssInject(){
-  return gulp.src('./app/temp/styles/styles.css')
+  return gulp.src('./docs/temp/styles/styles.css')
     .pipe(browserSync.stream());
 }
 function watch(cb){
     browserSync.init({
       notify:false,
       server: {
-            baseDir: "app"
+            baseDir: "docs"
         }
     });
-    gulp.watch('./app/index.html',function(cb){
+    gulp.watch('./docs/index.html',function(cb){
       browserSync.reload();
       cb();
     });
-    gulp.watch('./app/assets/styles/**/*.css',gulp.series(styles,cssInject));
+    gulp.watch('./docs/assets/styles/**/*.css',gulp.series(styles,cssInject));
     cb();
 }
 exports.watch=watch;
